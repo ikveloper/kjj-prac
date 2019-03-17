091549ac48a0b7a94cd3858d7a3d1c2e2309598a
ikveloper <ikveloper@gmail.com>
2019-03-16 22:23:01 -0400

    new repo

---
+++ app.js
@@ -0,0 +1,45 @@
+const kijiji = require("kijiji-scraper");
+ 
+let options = {
+    minResults: 10,
+    maxResults: -1
+};
+ 
+let params = {
+    locationId: 1700272,  // Same as kijiji.locations.ONTARIO.GTA
+    categoryId: 760,  
+    sortByName: "priceAsc",  // Show the cheapest listings first
+    keywords: 'iphone',
+    maxPrice: 1000,
+    minPrice: 400
+};
+ 
+// Scrape using returned promise
+kijiji.search(params, options).then(function(ads) {
+    // Use the ads array
+    const arr =[];
+    for (let i = 0; i < ads.length; ++i) {
+        //console.log('----------------------------------');
+        //console.log(ads[i].title);
+        //console.log(ads[i].image);
+        //console.log(ads[i].attributes.price);
+        arr.push(ads[i]);
+
+    }
+    arr.forEach(elem) 
+        console.log(elem.attributes.price);
+    
+}).catch(console.error);
+ 
+// Scrape using optional callback parameter
+function callback(err, ads) {
+    if (!err) {
+        // Use the ads array
+        for (let i = 0; i < ads.length; ++i) {
+            console.log(ads[i].title);
+            
+        }
+    }
+}
+kijiji.search(params, options, callback);
+
